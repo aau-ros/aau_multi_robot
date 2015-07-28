5fc5385f14f738a44b45d01b8fea5278dd3df901
Micha Rappaport <micha.rappaport@aau.at>
2015-07-28 15:14:27 +0200

    don't reduce battery charge when currently recharging

--- battery_simulation/src/BatteryState.cpp
+++ battery_simulation/src/BatteryState.cpp
@@ -17,16 +17,21 @@ double actual_charge;
 double x_linear, z_angular;
 double moving_percentage;
 double avg_speed;
+bool charging;
 
 battery_simulation::Battery battery_state;
 
-// Callback message
+void charge_starting_callback(const std_msgs::Empty::ConstPtr &msg) {
+    charging = true;
+}
+
 void charge_complete_callback(const std_msgs::Empty::ConstPtr &msg) {
     //When the charging is done we reset the battery state to 100%.
     ROS_INFO("charging done.");
     battery_state.percent = 100;
     battery_state.remaining_distance = avg_speed * moving_percentage * battery_charge/moving_consumption*3600;
     actual_charge = battery_charge;
+    charging = false;
 }
 
 void cmd_callback(const geometry_msgs::Twist &msg){
@@ -43,7 +48,6 @@ int main(int argc, char** argv) {
     battery_state.percent = 100;
     battery_state.remaining_distance = avg_speed * battery_charge/moving_consumption*3600;
 
-
     double rate = 0.5; // Hz
     double temp;
     double moving_time;
@@ -51,9 +55,11 @@ int main(int argc, char** argv) {
 
     standing_time = 0;
     moving_time = 0;
+    charging = false;
 
     ros::Publisher battery_pub = n.advertise<battery_simulation::Battery>("battery_state", 1000);
-    ros::Subscriber charge_sub = n.subscribe("charge_complete", 1000, charge_complete_callback);
+    ros::Subscriber charge1_sub = n.subscribe("going_to_recharge", 1, charge_starting_callback);
+    ros::Subscriber charge2_sub = n.subscribe("charge_complete", 1000, charge_complete_callback);
     ros::Subscriber cmd_sub = n.subscribe("cmd_vel", 1000, cmd_callback);
 
     n.getParam("ChargeSignal/energy/battery_charge",avg_speed);
@@ -68,42 +74,46 @@ int main(int argc, char** argv) {
 
         battery_pub.publish(battery_state);
 
-        /*
-        In the cmd_vel message there are only two important parameters, the x-linear value and the z-angular value.
-        When they are zero the robots stands still. If the x-value is unequal to zero then the robot moves forward or
-        backward and if the z-value is unequal to zero the robot rotates.
-        When the robot stands still it consumes less energy and when it moves it consumes more energy.
-        */
-        if(x_linear == 0 && z_angular == 0){
-            if (battery_state.percent > 0){
-                temp = actual_charge - standing_consumption / (rate * 3600 );
-                actual_charge = temp;
+        // only use energy if robot is not recharging
+        if(charging == false)
+        {
+            /*
+            In the cmd_vel message there are only two important parameters, the x-linear value and the z-angular value.
+            When they are zero the robots stands still. If the x-value is unequal to zero then the robot moves forward or
+            backward and if the z-value is unequal to zero the robot rotates.
+            When the robot stands still it consumes less energy and when it moves it consumes more energy.
+            */
+            if(x_linear == 0 && z_angular == 0){
+                if (battery_state.percent > 0){
+                    temp = actual_charge - standing_consumption / (rate * 3600 );
+                    actual_charge = temp;
+                }
+                standing_time += 1/rate;
+            }else{
+                if (battery_state.percent > 0){
+                    temp = actual_charge - moving_consumption / (rate * 3600 );
+                    actual_charge = temp;
+                }
+                moving_time += 1/rate;
             }
-            standing_time += 1/rate;
-        }else{
-            if (battery_state.percent > 0){
-                temp = actual_charge - moving_consumption / (rate * 3600 );
-                actual_charge = temp;
+
+            // percentage of battery charge remaining
+            battery_state.percent = (actual_charge * 100) / battery_charge;
+
+            /* The remaining distance depends on the percentage of time the robot is moving.
+            * In the beginning this calculation is very inaccurate. We set the remaining distance to a high value
+            * to avoid that the robot returns too early to the home point for recharging.
+            */
+            if(moving_time < 30){
+                moving_percentage = 0.25;
             }
-            moving_time += 1/rate;
+            else{
+                moving_percentage = moving_time / (standing_time+moving_time);
+            }
+            // remaining distance the robot can still travel
+            //                                                                                               max time the robot can travel with full battery
+            battery_state.remaining_distance = battery_state.percent/100.0 * avg_speed * moving_percentage * (float)battery_charge/moving_consumption*3600;
         }
-
-        // percentage of battery charge remaining
-        battery_state.percent = (actual_charge * 100) / battery_charge;
-
-        /* The remaining distance depends on the percentage of time the robot is moving.
-         * In the beginning this calculation is very inaccurate. We set the remaining distance to a high value
-         * to avoid that the robot returns too early to the home point for recharging.
-         */
-        if(moving_time < 30){
-            moving_percentage = 0.25;
-        }
-        else{
-            moving_percentage = moving_time / (standing_time+moving_time);
-        }
-        // remaining distance the robot can still travel
-        //                                                                                               max time the robot can travel with full battery
-        battery_state.remaining_distance = battery_state.percent/100.0 * avg_speed * moving_percentage * (float)battery_charge/moving_consumption*3600;
 
 		ros::spinOnce();
 		loop_rate.sleep();
