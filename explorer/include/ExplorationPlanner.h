4d8bc61821ff8e98e1409f7c735bb4e0d91c59b8
Micha Rappaport <micha.rappaport@aau.at>
2015-08-25 16:32:19 +0200

    completely removed all references to adhoc communication

--- explorer/include/ExplorationPlanner.h
+++ explorer/include/ExplorationPlanner.h
@@ -10,12 +10,12 @@
 #include <costmap_2d/costmap_2d.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <adhoc_communication/ExpFrontier.h>
+/*#include <adhoc_communication/ExpFrontier.h>
 #include <adhoc_communication/ExpCluster.h>
 #include <adhoc_communication/ExpAuction.h>
 #include <adhoc_communication/MmPoint.h>
 #include <adhoc_communication/MmListOfPoints.h>
-#include <map_merger/TransformPoint.h>
+#include <map_merger/TransformPoint.h>*/
 
 namespace explorationPlanner
 {
@@ -136,19 +136,19 @@ namespace explorationPlanner
             std::vector<double> last_goal_position_y;
 
             std::vector<cluster_t> clusters;
-            std::vector<adhoc_communication::ExpCluster> unrecognized_occupied_clusters;
+            //std::vector<adhoc_communication::ExpCluster> unrecognized_occupied_clusters;
 
             std::vector<transform_point_t> transformedPointsFromOtherRobot_frontiers, transformedPointsFromOtherRobot_visited_frontiers;
 
             std::vector<int> already_used_ids, id_previously_detected;
 
-            adhoc_communication::MmListOfPoints other_robots_positions;
+            //adhoc_communication::MmListOfPoints other_robots_positions;
 
             tf::TransformListener listener;
             tf::StampedTransform transform;
             tf::Stamped < tf::Pose > robotPose;
 
-            map_merger::TransformPoint service_message;
+            //map_merger::TransformPoint service_message;
 
             navfn::NavfnROS nav;
 
@@ -189,20 +189,20 @@ namespace explorationPlanner
 
             ExplorationPlanner(int robot_id, bool robot_prefix_empty, std::string robot_name_parameter);
             void printFrontiers();
-            bool respondToAuction(std::vector<requested_cluster_t> requested_cluster_ids, int auction_id_number);
+            //bool respondToAuction(std::vector<requested_cluster_t> requested_cluster_ids, int auction_id_number);
             bool clusterIdToElementIds(int cluster_id, std::vector<transform_point_t>* occupied_ids);
-            bool initialize_auctioning(std::vector<double> *final_goal);
-            bool auctioning(std::vector<double> *final_goal, std::vector<int> *clusters_available_in_pool, std::vector<std::string> *robot_str_name);
-            bool selectClusterBasedOnAuction(std::vector<double> *goal, std::vector<int> *cluster_in_use_already_count, std::vector<std::string> *robot_str_name_to_return);
-            bool InitSelectClusterBasedOnAuction(std::vector<double> *goal);
-            int calculateAuctionBID(int cluster_number, std::string strategy);
+            //bool initialize_auctioning(std::vector<double> *final_goal);
+            //bool auctioning(std::vector<double> *final_goal, std::vector<int> *clusters_available_in_pool, std::vector<std::string> *robot_str_name);
+            //bool selectClusterBasedOnAuction(std::vector<double> *goal, std::vector<int> *cluster_in_use_already_count, std::vector<std::string> *robot_str_name_to_return);
+            //bool InitSelectClusterBasedOnAuction(std::vector<double> *goal);
+            //int calculateAuctionBID(int cluster_number, std::string strategy);
             std::string lookupRobotName(int robot_name_int);
             void trajectory_plan_10_frontiers();
             void trajectory_plan_store(double x, double y);
             int trajectory_plan(double x, double y);
             int trajectory_plan(double start_x, double start_y, double target_x, double target_y);
             void Callbacks();
-            int checkClustersID(adhoc_communication::ExpCluster cluster_to_check);
+            //int checkClustersID(adhoc_communication::ExpCluster cluster_to_check);
             bool determine_goal(int strategy, std::vector<double> *final_goal, int count, int actual_cluster_id, std::vector<std::string> *robot_str_name);
             void sort(int strategy);
             void sort_distance(bool energy_above_th);
@@ -225,20 +225,20 @@ namespace explorationPlanner
             bool removeStoredFrontier(int id, std::string detected_by_robot_str);
             bool removeVisitedFrontier(int id, std::string detected_by_robot_str);
             bool removeUnreachableFrontier(int id, std::string detected_by_robot_str);
-            bool publish_frontier_list();
-            bool publish_visited_frontier_list();
-            bool publish_negotiation_list(frontier_t negotiation_frontier, int cluster);
-            bool subscribe_negotiation_list();
+            //bool publish_frontier_list();
+            //bool publish_visited_frontier_list();
+            //bool publish_negotiation_list(frontier_t negotiation_frontier, int cluster);
+            //bool subscribe_negotiation_list();
             bool subscribe_frontier_list();
             bool subscribe_visited_frontier_list();
-            void negotiationCallback(const adhoc_communication::ExpFrontier::ConstPtr& msg);
-            void visited_frontierCallback(const adhoc_communication::ExpFrontier::ConstPtr& msg);
-            void frontierCallback(const adhoc_communication::ExpFrontier::ConstPtr& msg);
-            void auctionCallback(const adhoc_communication::ExpAuction::ConstPtr& msg);
-            void positionCallback(const adhoc_communication::MmListOfPoints::ConstPtr& msg);
+            //void negotiationCallback(const adhoc_communication::ExpFrontier::ConstPtr& msg);
+            //void visited_frontierCallback(const adhoc_communication::ExpFrontier::ConstPtr& msg);
+            //void frontierCallback(const adhoc_communication::ExpFrontier::ConstPtr& msg);
+            //void auctionCallback(const adhoc_communication::ExpAuction::ConstPtr& msg);
+            //void positionCallback(const adhoc_communication::MmListOfPoints::ConstPtr& msg);
             bool smartGoalBackoff(double x, double y, costmap_2d::Costmap2DROS *global_costmap, std::vector<double> *new_goal);
             void setRobotConfig(int name, double robot_home_position_x, double robot_home_position_y, std::string frame);
-            bool negotiate_Frontier(double x, double y, int detected_by, int id, int cluster);
+            //bool negotiate_Frontier(double x, double y, int detected_by, int id, int cluster);
             bool clusterFrontiers();
             void visualize_Clusters();
             void visualize_Cluster_Cells();
@@ -246,8 +246,8 @@ namespace explorationPlanner
             bool transformToOwnCoordinates_frontiers();
             bool transformToOwnCoordinates_visited_frontiers();
 
-            bool sendToMulticast(std::string multi_cast_group, adhoc_communication::ExpFrontier frontier_to_send, std::string topic);
-            bool sendToMulticastAuction(std::string multi_cast_group, adhoc_communication::ExpAuction auction_to_send, std::string topic);
+            //bool sendToMulticast(std::string multi_cast_group, adhoc_communication::ExpFrontier frontier_to_send, std::string topic);
+            //bool sendToMulticastAuction(std::string multi_cast_group, adhoc_communication::ExpAuction auction_to_send, std::string topic);
 
 
         private:
