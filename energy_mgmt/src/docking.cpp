2cafddea5156e1351bceae7f07c1c7e1fbe69258
Fabrizio Casati <fabrizio1.casati@mail.polimi.it>
2017-05-24 15:28:28 +0200

    Remove logging text

--- energy_mgmt/src/docking.cpp
+++ energy_mgmt/src/docking.cpp
@@ -2966,7 +2966,7 @@ void docking::test_2(const std_msgs::Empty &msg) {
 }
 
 bool docking::distance_robot_frontier_on_graph_callback(explorer::Distance::Request &req, explorer::Distance::Response &res) {
-    ROS_ERROR("called!");
+    //ROS_ERROR("called!");
     return true;
     
     int index_closest_ds_to_frontier, index_closest_ds_to_robot;
