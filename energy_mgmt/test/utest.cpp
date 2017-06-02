f2aaaab6e74762d6723fb475b0eba50e7c83ff1b
Fabrizio Casati <fabrizio1.casati@mail.polimi.it>
2017-06-02 18:35:03 +0200

    Add test file

---
+++ energy_mgmt/test/utest.cpp
@@ -0,0 +1,22 @@
+// Bring in my package's API, which is what I'm testing
+#include "docking.h"
+// Bring in gtest
+#include <gtest/gtest.h>
+
+// Declare a test
+TEST(TestSuite, testCase1)
+{
+    ; //<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+}
+
+// Declare another test
+TEST(TestSuite, testCase2)
+{
+    ; //<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+}
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
