81ba6645df7a1fcbf050b2bab9f2104286895829
Fabrizio Casati <fabrizio1.casati@mail.polimi.it>
2017-06-27 18:22:29 +0200

    Fix bug with battery state

--- explorer/src/explorer.cpp
+++ explorer/src/explorer.cpp
@@ -675,7 +675,7 @@ class Explorer
                 if(conservative_maximum_available_distance < 0 || conservative_maximum_available_distance < 0.95*conservative_available_distance(next_available_distance)) {
                     ROS_INFO("setting maximum distance");
 //                    conservative_maximum_available_distance = conservative_available_distance(next_available_distance); 
-                    conservative_maximum_available_distance = 0.95*conservative_available_distance(available_distance);
+                    conservative_maximum_available_distance = 0.95*conservative_available_distance(next_available_distance);
                 }
                    
                 if(use_full_energy) {
