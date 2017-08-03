b2d60154705a261cd4ef6efcc9051508cb5db0ce
Fabrizio Casati <fabrizio1.casati@mail.polimi.it>
2017-08-03 14:52:17 +0200

    Minor changes

--- energy_mgmt/src/docking.cpp
+++ energy_mgmt/src/docking.cpp
@@ -2239,7 +2239,7 @@ void docking::start_new_auction()
     
     if(robot_is_auctioning) {
         log_major_error("robot_is_auctioning is true, but shoud be false!!");
-        timer_restart_auction.stop();
+        timer_finish_auction.stop();
         auction_winner = false;
         robot_is_auctioning = false;
         expired_own_auction = true;
