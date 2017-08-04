ec57173ba3f84dd953e6869a034afa5c6889bddb
Fabrizio Casati <fabrizio1.casati@mail.polimi.it>
2017-08-04 17:28:19 +0200

    Minor changes

--- energy_mgmt/src/docking.cpp
+++ energy_mgmt/src/docking.cpp
@@ -2603,8 +2603,11 @@ void docking::update_robot_state()  // TODO(minor) simplify
 
     
     // sanity check
-    if(robot_state == in_queue && (ros::Time::now() - changed_state_time > ros::Duration(3*60)))
+    if(robot_state == in_queue && (ros::Time::now() - changed_state_time > ros::Duration(3*60))) {
         log_major_error("robot stucked in queue!!!!");
+        pub_lost_own_auction.publish(msg);  
+        ROS_INFO("pub_lost_own_auction");
+    }
     
     // check expired auctions
     for(auto it = auctions.begin(); it != auctions.end(); )
