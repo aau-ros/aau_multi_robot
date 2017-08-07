f37489744487de7cb95845ad4973eaaa16cdc071
fabriziocasati <casati.fabrizio@gmail.com>
2017-08-07 21:20:25 +0200

    Minor changes

--- energy_mgmt/src/computer.cpp
+++ energy_mgmt/src/computer.cpp
@@ -3,7 +3,7 @@
 Computer2::Computer2(explorer::battery_state *b) {
     this->b = b; //TODO bas names
     ros::NodeHandle nh_tilde("~");
-    nh_tilde.getParam("speed_avg", speed_avg_init); //TODO speed_avg maybe is not a good name
+    nh_tilde.getParam("speed_avg_init", speed_avg_init); //TODO use config file instead of yaml file for the parameters
     nh_tilde.getParam("power_charging", power_charging); // W (i.e, watt)
     nh_tilde.getParam("power_per_speed", power_per_speed);     // W/(m/s)
     nh_tilde.getParam("power_moving_fixed_cost", power_moving_fixed_cost);     // W/(m/s)
--- robot_state/include/robot_state/robot_state.h
+++ robot_state/include/robot_state/robot_state.h
@@ -10,20 +10,20 @@ namespace robot_state
     //TODO add classes for Visitor
     enum robot_state_t
     {
-        INITIALIZING,
-        CHOOSING_ACTION,
-        COMPUTING_NEXT_GOAL,              // the robot is computing which is the next goal (frontier, docking station, ...)
-        MOVING_TO_FRONTIER,              // the robot is moving to a selected frontier
-        GOING_CHECKING_VACANCY, // the robot is approaching a DS to check if it is actually free
-        CHECKING_VACANCY,
-        GOING_CHARGING,
-        CHARGING,
-        CHARGING_COMPLETED,
-        CHARGING_ABORTED,
-        LEAVING_DS,
-        GOING_IN_QUEUE,
-        IN_QUEUE,
-        AUCTIONING
+        INITIALIZING,           // the robot has just been activated
+        CHOOSING_ACTION,        // the robot is checking if it won an auction and so it has to recharge or if it can try to check if it can reach a new frontier
+        COMPUTING_NEXT_GOAL,    // the robot is computing which is the next goal (frontier, docking station, ...)
+        MOVING_TO_FRONTIER,     // the robot is moving to a selected frontier
+        GOING_CHECKING_VACANCY, // the robot is approaching the optimal DS to check if it is actually free
+        CHECKING_VACANCY,       // the robot is in proximity of the optimal DS and it's checking if it's actually free
+        GOING_CHARGING,         // the robot has checked that the optimal DS is actually free, and so it is going to occupy it to recharge
+        CHARGING,               // the robot is charging at the optimal DS
+        CHARGING_COMPLETED,     // the robot has just finished charging
+        CHARGING_ABORTED,       // the charging has been aborted due to action lost
+        LEAVING_DS,             // the robot is leaving the optimal DS
+        GOING_IN_QUEUE,         // the robot has lost an auction started by itself, and so it has to queue
+        IN_QUEUE,               // the robot is in queue, waiting for the optimal DS to because vacant
+        AUCTIONING              // the robot has just started its own auction
     };
 
 }
