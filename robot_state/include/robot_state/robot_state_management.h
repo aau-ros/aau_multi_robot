17ae8478762241a9dba2612612003bad915c9a87
fabriziocasati <casati.fabrizio@gmail.com>
2017-08-19 14:16:06 +0200

    Add tests for RobotStateApi

--- robot_state/include/robot_state/robot_state_management.h
+++ robot_state/include/robot_state/robot_state_management.h
@@ -11,23 +11,24 @@ class RobotStateApi
 {
 public:
     RobotStateApi();
-//    RobotState *getRobotState();
-//    bool getRobotState2();
-//    void setRobotState(RobotState *state);
-    RobotState *enumToState(unsigned int enum_val);
-    unsigned int stateToEnum(RobotState *state);
+    RobotState *getRobotState();
+    robot_state::robot_state_t getRobotStateEnum();
+    void setRobotState(RobotState *state);
+    void setRobotStateEnum(robot_state::robot_state_t state);
 
 private:
     std::unordered_map<unsigned int, RobotState*> enumToStateMap;
     std::unordered_map<RobotState*, unsigned int> stateToEnumMap;
-//    ros::ServiceClient get_robot_state_sc;
-//    ros::ServiceClient set_robot_state_sc;
+    ros::ServiceClient get_robot_state_sc;
+    ros::ServiceClient set_robot_state_sc;
 //    ros::ServiceClient try_to_lock_robot_state_ss;
 //    ros::ServiceClient unlock_robot_state_ss;
 
-//    void createServiceClients();
+    void createServiceClients();
     void fillMaps();
     void addStateToMaps(unsigned int enum_val, RobotState *state);
+    RobotState *enumToState(unsigned int enum_val);
+    unsigned int stateToEnum(RobotState *state);
 };
 
 #endif // ROBOT_STATE_MANAGEMENT_H
--- robot_state/src/robot_state_interface.cpp
+++ robot_state/src/robot_state_interface.cpp
@@ -2,22 +2,22 @@
 
 RobotStateApi::RobotStateApi() {//TODO RobotStatePackageApi and .cpp file name
     ROS_INFO("Creating instance of RobotStateApi");
-//    createServiceClients();
-//    fillMaps();
+    createServiceClients();
+    fillMaps();
     ROS_INFO("Instance correctly created");
 }
 
-//void RobotStateApi::createServiceClients() {
-//    ros::NodeHandle nh;
-//    set_robot_state_sc = nh.serviceClient<robot_state::SetRobotState>("robot_state/set_robot_state");
-//    get_robot_state_sc = nh.serviceClient<robot_state::GetRobotState>("robot_state/get_robot_state");
+void RobotStateApi::createServiceClients() {
+    ros::NodeHandle nh;
+    set_robot_state_sc = nh.serviceClient<robot_state::SetRobotState>("robot_state/set_robot_state");
+    get_robot_state_sc = nh.serviceClient<robot_state::GetRobotState>("robot_state/get_robot_state");
 //    try_to_lock_robot_state_ss = nh.serviceClient<robot_state::SetRobotState>("robot_state/try_to_lock_robot_state");
 //    unlock_robot_state_ss = nh.serviceClient<robot_state::SetRobotState>("robot_state/unlock_robot_state");
-//}
+}
 
 void RobotStateApi::fillMaps() { //TODO complete stateMap
-//    addStateToMaps(robot_state::INITIALIZING, new InitializingState());
-//    addStateToMaps(robot_state::CHOOSING_ACTION, new ChoosingActionState());
+    addStateToMaps(robot_state::INITIALIZING, new InitializingState());
+    addStateToMaps(robot_state::CHOOSING_ACTION, new ChoosingActionState());
 }
 
 void RobotStateApi::addStateToMaps(unsigned int enum_val, RobotState *state) {
@@ -25,16 +25,25 @@ void RobotStateApi::addStateToMaps(unsigned int enum_val, RobotState *state) {
     stateToEnumMap.insert({state, enum_val});
 }
 
-//RobotState *RobotStateApi::getRobotState() { //TODO raise exception //TODO put locks
-//    ROS_ERROR("calling");  
-//    robot_state::GetRobotState get_srv_msg;
-//    bool call_succeeded = get_robot_state_sc.call(get_srv_msg);
-//    while(!call_succeeded) { //TODO max number of retries
-//        ROS_ERROR("call failed");   
-//        call_succeeded = get_robot_state_sc.call(get_srv_msg);
-//    }
-//    return enumToStateMap.at(get_srv_msg.response.robot_state); //TODO safety check to avoid out_of_range
-//}
+RobotState *RobotStateApi::getRobotState() { //TODO raise exception //TODO put locks
+    robot_state::GetRobotState get_srv_msg;
+    bool call_succeeded = get_robot_state_sc.call(get_srv_msg);
+    while(!call_succeeded) { //TODO max number of retries
+        ROS_ERROR("call failed");   
+        call_succeeded = get_robot_state_sc.call(get_srv_msg);
+    }
+    return enumToStateMap.at(get_srv_msg.response.robot_state); //TODO safety check to avoid out_of_range
+}
+
+robot_state::robot_state_t RobotStateApi::getRobotStateEnum() { //TODO raise exception //TODO put locks
+    robot_state::GetRobotState get_srv_msg;
+    bool call_succeeded = get_robot_state_sc.call(get_srv_msg);
+    while(!call_succeeded) { //TODO max number of retries
+        ROS_ERROR("call failed");   
+        call_succeeded = get_robot_state_sc.call(get_srv_msg);
+    }
+    return static_cast<robot_state::robot_state_t>(get_srv_msg.response.robot_state); //TODO safety check to avoid out_of_range
+}
 
 //bool RobotStateApi::getRobotState2() { //TODO raise exception
 //    ROS_ERROR("calling");  
@@ -42,15 +51,25 @@ void RobotStateApi::addStateToMaps(unsigned int enum_val, RobotState *state) {
 //    return get_robot_state_sc.call(get_srv_msg);
 //}
 
-//void RobotStateApi::setRobotState(RobotState *state) { //TODO raise exception
-//    robot_state::SetRobotState set_srv_msg;
-//    set_srv_msg.request.robot_state = stateToEnumMap.at(state);
-//    bool call_succeeded = set_robot_state_sc.call(set_srv_msg);
-//    while(!call_succeeded) { //TODO max number of retries
-//        ROS_ERROR("call failed");   
-//        call_succeeded = set_robot_state_sc.call(set_srv_msg); //TODO check if the state was set (it could fail due to a lock
-//    }
-//}
+void RobotStateApi::setRobotState(RobotState *state) { //TODO raise exception
+    robot_state::SetRobotState set_srv_msg;
+    set_srv_msg.request.robot_state = stateToEnumMap.at(state);
+    bool call_succeeded = set_robot_state_sc.call(set_srv_msg);
+    while(!call_succeeded) { //TODO max number of retries
+        ROS_ERROR("call failed");   
+        call_succeeded = set_robot_state_sc.call(set_srv_msg); //TODO check if the state was set (it could fail due to a lock
+    }
+}
+
+void RobotStateApi::setRobotStateEnum(robot_state::robot_state_t state) { //TODO raise exception
+    robot_state::SetRobotState set_srv_msg;
+    set_srv_msg.request.robot_state = state;
+    bool call_succeeded = set_robot_state_sc.call(set_srv_msg);
+    while(!call_succeeded) { //TODO max number of retries
+        ROS_ERROR("call failed");   
+        call_succeeded = set_robot_state_sc.call(set_srv_msg); //TODO check if the state was set (it could fail due to a lock
+    }
+}
 
 RobotState* RobotStateApi::enumToState(unsigned int enum_val) {
     return enumToStateMap.at(enum_val);
